bce6023d9c10fc0474f4abd17c6b1d1763a656e0
roq-ai <>
2023-10-18 11:17:53 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,22 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+  ownerAbilities: string[];
+  customerAbilities: string[];
+  getQuoteUrl: string;
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Owner'],
+  customerRoles: [],
+  tenantRoles: ['Owner', 'HR Manager', 'Employee', 'Administrator', 'Applicant'],
+  tenantName: 'Company',
+  applicationName: 'HR Management Application',
+  addOns: ['file upload', 'chat', 'notifications', 'file'],
+  customerAbilities: [],
+  ownerAbilities: ['Manage interviews', 'Manage user information', 'Manage company information'],
+  getQuoteUrl: 'https://app.roq.ai/proposal/415c7e3a-eb45-4d7d-8c5e-60cfb49ad848',
+};
---
+++ src/lib/roq/roq-hooks.ts
@@ -0,0 +1,265 @@
+/* eslint-disable */
+import useSWR from 'swr';
+import type { Prisma } from './types';
+import { DefaultArgs, GetFindResult } from './types/runtime/library';
+import { useRoqClient } from './roq-client-provider';
+import { SWRRequestOptions, RequestOptions } from './roq-sdk';
+
+export function useCompanyFindMany<
+  T extends Prisma.companyFindManyArgs,
+  R extends GetFindResult<Prisma.$companyPayload<DefaultArgs>, T>[],
+>(
+  args?: Prisma.SelectSubset<T, Prisma.companyFindManyArgs>,
+  options?: RequestOptions,
+  swrOptions?: SWRRequestOptions<R>,
+) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useCompanyFindMany', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.company.findMany(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
+
+export function useCompanyCount<T extends Prisma.companyCountArgs, R extends number>(
+  args?: Prisma.SelectSubset<T, Prisma.companyCountArgs>,
+  options?: RequestOptions,
+  swrOptions?: SWRRequestOptions<R>,
+) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useCompanyCount', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.company.count(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
+
+export function useCompanyFindManyWithCount<
+  T extends Prisma.companyFindManyArgs,
+  R extends { data: GetFindResult<Prisma.$companyPayload<DefaultArgs>, T>[]; count: number },
+>(
+  args?: Prisma.SelectSubset<T, Prisma.companyFindManyArgs>,
+  options?: RequestOptions,
+  swrOptions?: SWRRequestOptions<R>,
+) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useCompanyFindManyWithCount', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.company.findManyWithCount(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
+
+export function useCompanyFindFirst<
+  T extends Prisma.companyFindFirstArgs,
+  R extends GetFindResult<Prisma.$companyPayload<DefaultArgs>, T>,
+>(
+  args?: Prisma.SelectSubset<T, Prisma.companyFindFirstArgs>,
+  options?: RequestOptions,
+  swrOptions?: SWRRequestOptions<R>,
+) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useCompanyFindFirst', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.company.findFirst(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
+
+export function useInterviewsFindMany<
+  T extends Prisma.interviewsFindManyArgs,
+  R extends GetFindResult<Prisma.$interviewsPayload<DefaultArgs>, T>[],
+>(
+  args?: Prisma.SelectSubset<T, Prisma.interviewsFindManyArgs>,
+  options?: RequestOptions,
+  swrOptions?: SWRRequestOptions<R>,
+) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useInterviewsFindMany', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.interviews.findMany(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
+
+export function useInterviewsCount<T extends Prisma.interviewsCountArgs, R extends number>(
+  args?: Prisma.SelectSubset<T, Prisma.interviewsCountArgs>,
+  options?: RequestOptions,
+  swrOptions?: SWRRequestOptions<R>,
+) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useInterviewsCount', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.interviews.count(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
+
+export function useInterviewsFindManyWithCount<
+  T extends Prisma.interviewsFindManyArgs,
+  R extends { data: GetFindResult<Prisma.$interviewsPayload<DefaultArgs>, T>[]; count: number },
+>(
+  args?: Prisma.SelectSubset<T, Prisma.interviewsFindManyArgs>,
+  options?: RequestOptions,
+  swrOptions?: SWRRequestOptions<R>,
+) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useInterviewsFindManyWithCount', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.interviews.findManyWithCount(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
+
+export function useInterviewsFindFirst<
+  T extends Prisma.interviewsFindFirstArgs,
+  R extends GetFindResult<Prisma.$interviewsPayload<DefaultArgs>, T>,
+>(
+  args?: Prisma.SelectSubset<T, Prisma.interviewsFindFirstArgs>,
+  options?: RequestOptions,
+  swrOptions?: SWRRequestOptions<R>,
+) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useInterviewsFindFirst', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.interviews.findFirst(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
+
+export function useUserFindMany<
+  T extends Prisma.userFindManyArgs,
+  R extends GetFindResult<Prisma.$userPayload<DefaultArgs>, T>[],
+>(args?: Prisma.SelectSubset<T, Prisma.userFindManyArgs>, options?: RequestOptions, swrOptions?: SWRRequestOptions<R>) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useUserFindMany', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.user.findMany(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
+
+export function useUserCount<T extends Prisma.userCountArgs, R extends number>(
+  args?: Prisma.SelectSubset<T, Prisma.userCountArgs>,
+  options?: RequestOptions,
+  swrOptions?: SWRRequestOptions<R>,
+) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useUserCount', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.user.count(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
+
+export function useUserFindManyWithCount<
+  T extends Prisma.userFindManyArgs,
+  R extends { data: GetFindResult<Prisma.$userPayload<DefaultArgs>, T>[]; count: number },
+>(args?: Prisma.SelectSubset<T, Prisma.userFindManyArgs>, options?: RequestOptions, swrOptions?: SWRRequestOptions<R>) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useUserFindManyWithCount', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.user.findManyWithCount(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
+
+export function useUserFindFirst<
+  T extends Prisma.userFindFirstArgs,
+  R extends GetFindResult<Prisma.$userPayload<DefaultArgs>, T>,
+>(
+  args?: Prisma.SelectSubset<T, Prisma.userFindFirstArgs>,
+  options?: RequestOptions,
+  swrOptions?: SWRRequestOptions<R>,
+) {
+  const roq = useRoqClient();
+  const key = JSON.stringify(['useUserFindFirst', args || {}]);
+  return useSWR<R, Error>(
+    key,
+    async () => {
+      const result = await roq.user.findFirst(args, options);
+      return result as R;
+    },
+    {
+      ...swrOptions,
+      fallbackData: swrOptions?.initialData ?? swrOptions?.fallbackData,
+    },
+  );
+}
